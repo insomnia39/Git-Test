21cece2fa250a03599e523336d4e27139b3aa140
insomnia39 <fakhrialbari.dev@outlook.com>
2023-08-11 09:26:44 +0700

    Add project files.

---
+++ Git-Test/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Git_Test
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            var n = 10;
+            var t = 20;
+            var z = 2;
+            var total = (n * t) / z;
+        }
+    }
+}
