f5f018cb6c28802bfae45d86e0d5f8927f95df5b
insomnia39 <fakhrialbari.dev@outlook.com>
2023-08-11 09:30:42 +0700

    Add 2

--- Git-Test/Program.cs
+++ Git-Test/Program.cs
@@ -8,7 +8,8 @@
             var n = 10;
             var t = 20;
             var z = 2;
-            var total = (n * t) / z;
+            var total = ((n * t) + 2) / z;
+            Console.WriteLine("Done");
         }
     }
 }
