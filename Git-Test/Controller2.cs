b52b079d5d4ba1592d336b02eb0935f5b724f189
insomnia39 <fakhrialbari.dev@outlook.com>
2023-08-11 10:06:50 +0700

    Update Controller 2

--- Git-Test/Controller2.cs
+++ Git-Test/Controller2.cs
@@ -13,6 +13,12 @@ namespace Git_Test
             
         }
 
+        public void ReadFromDB(int a, int b)
+        {
+            int total = Logic.Add(a, b);
+            int least = Logic.Substract(a, b);
+        }
+
         public void RemoveFromDB(int a, int b)
         {
             int total = Logic.Substract(a, b);
