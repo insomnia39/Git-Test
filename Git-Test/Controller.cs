498f2529d479570b9cbb303b6ecabb79e1d97e1a
insomnia39 <fakhrialbari.dev@outlook.com>
2023-08-11 10:04:59 +0700

    Update controller

--- Git-Test/Controller.cs
+++ Git-Test/Controller.cs
@@ -1,21 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Git_Test
+﻿namespace Git_Test
 {
     public class Controller
     {
         public Controller(String args)
         {
-            
+
         }
 
         public void AddToDatabase(int a, int b)
         {
-            int total = Logic.Add(a, b);
+            double total = Logic.Multiply(Logic.Add(a, b), b);
         }
     }
 }
