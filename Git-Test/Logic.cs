7f1afdded9be1eeb1a466a7e7a1cfd3ce7f75681
insomnia39 <fakhrialbari.dev@outlook.com>
2023-08-11 09:52:57 +0700

    Add logic class

---
+++ Git-Test/Logic.cs
@@ -0,0 +1,15 @@
+﻿namespace Git_Test
+{
+    public static class Logic
+    {
+        public static int Add(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+
+        public static int Substract(int n1, int n2)
+        {
+            return n1 - n2;
+        }
+    }
+}
